f02a1c14ce7c850c5d1928f15608e5b28a51ec15
mchrzanka <mayachrzanowska@live.com>
2021-11-09 12:29:28 -0700

    gulp redirect

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 //Gulp runs on top of node. We're writing ES modules (common for browser) that use import to import modules, and export default to export modules. Node is written in a different module system, called common.js
 
-const { src, dest } = require('gulp');
+const { src, dest, series } = require('gulp');
 
 const static = function () {
 	//pipe takes the command on the left hand side and passes it into the brackets
 	return src('src/static/data/*.*').pipe(dest('dist/data'));
 };
 
-exports.default = static;
+function redirect() {
+	return src('./_redirects').pipe(dest('./dist'));
+}
+
+exports.default = series(static, redirect);
